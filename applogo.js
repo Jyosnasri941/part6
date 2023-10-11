b218d553a0cabb65b6ee56ce984d19e130512e8e
Jyosnasri941 <147608548+Jyosnasri941@users.noreply.github.com>
2023-10-11 16:29:22 +0530

    Add files via upload

---
+++ applogo.js
@@ -0,0 +1,52 @@
+minimport ("https://fonts.googleapis.com/css2?family=Climate+Crisis&family=Lora:ital@1&family=Quicksand&display=swap");
+App.js 
+  display; flex;
+  flex-direction; column;
+  align-items; center;
+  justify-content; center;
+  margin-top;  300;
+  margin-botto; 150;
+  margin-left; 50;
+  margin-right; 50;
+  background-color; rgb(64, 91, 79);
+  text-decoration; solid;
+  border-radius; 100;
+  font-size; xx-large;
+  font-family; Lora, serif;
+  color; rgb(12, 10, 24);
+body; 
+  background-color; rgba(38, 74, 46, 0.986);
+App-logo;
+  height; 2;
+  pointer-events; none;
+button;
+  background-color; 
+  color; white;
+  padding; 10;
+  border-radius; 400;
+  box-shadow;  0; 2394255
+  font-size; 1;
+  cursor; pointer;
+button; however: {
+  background-color; 45
+media (prefers-reduced-motion -preference) 
+  App-logo 
+    animation; App-logo-spin ; {
+  }
+App-header;
+  background-color; 282
+  min-height; 100;
+  display; flex;
+  flex-direction; column;
+  align-items; center;
+  justify-content; center;
+  font-size; calc(10 + 2);
+  color; white;
+App-link;
+  color; 61
+keyframes: App-logo-spin
+  from 
+    transform; rotate(0);
+  to 
+    transform; rotate(360);
+}
